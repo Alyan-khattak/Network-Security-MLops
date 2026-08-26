809f15b78ed5fe02da71d333d1e6a80cdfc66a0d
Alyan-khattak <alyankhattake@gmail.com>
2026-08-26 19:14:02 +0500

    Write functions to save obj and array

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -23,7 +23,8 @@ import yaml                          # YAML files parse karne ke liye
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-
+import numpy as np
+import dill
 
 # ══════════════════════════════════════════════════════════════════
 # FUNCTION 1: read_yaml_file
@@ -94,6 +95,45 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+
+
+
+
+# ══════════════════════════════════════════════════════════════════
+# FUNCTION 3: save_numpy_array_data
+# ══════════════════════════════════════════════════════════════════
+def save_numpy_array_data(file_path:str, array: np.array):
+    try:
+        logging.info("Entered The Save numpy array  method of util.py to save ")
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+
+
+# ══════════════════════════════════════════════════════════════════
+# FUNCTION 4: save_object
+# ══════════════════════════════════════════════════════════════════
+def save_object(file_path:str, obj: object):
+    try:
+        logging.info("Entered The Save object method of util.py to save {obj} ")
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(file_obj, obj)
+
+        logging.info("Existed the mehod and saved : {obj} ")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+    
+
+
+
 
 
 # ─────────────────────────────────────────────────────────────────
